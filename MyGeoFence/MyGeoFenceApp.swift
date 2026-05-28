e64354e6e5498e8e1c372b70e9066a17d9efbfb0
ReggieGrant <grantreggie09@gmail.com>
2026-05-27 17:26:06 -0700

    Initial Commit

---
+++ MyGeoFence/MyGeoFenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGeoFenceApp.swift
+//  MyGeoFence
+//
+//  Created by Reginald Grant on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MyGeoFenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
